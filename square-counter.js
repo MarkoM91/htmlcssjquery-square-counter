752767b44f90e4de6212268f93a385cb3f24ae7f
MarkoM91 <markomedic91@gmail.com>
2019-03-04 14:59:21 +0100

    commit2

--- square-counter.js
+++ square-counter.js
@@ -9,10 +9,12 @@ green.click(function() {
 
  contatore -= 1;
  white.html(contatore);
+ white.css("background-color" , "rgb(120, 166, 14)");
 });
 
 red.click(function() {
   contatore += 1;
   white.html(contatore);
+  white.css("background-color" , "rgb(81, 6, 128)");
 
 });
