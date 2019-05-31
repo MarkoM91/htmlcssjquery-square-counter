90cb0ec664f8a5170ce8a2e805579b36dafb1dda
MarkoM91 <markomedic91@gmail.com>
2019-05-31 14:34:24 +0200

    commit4

--- square-counter.js
+++ square-counter.js
@@ -1,19 +1,65 @@
-var green = $(".green");
-var white = $(".white");
-var red = $(".red");
-var contatore = 0;
+function getRandom(min, max) {
+  var delta = max - min + 1;
+  var rnd = Math.floor(Math.random() * delta) + min;
 
+  return rnd;
+}
 
+function minusClick() {
+  var green = $(".green");
+  var white = $(".white");
 
-green.click(function() {
+  green.click(function() {
 
- contatore -= 1;
- white.html(contatore);
- white.toggleClass("whitepurple" , "white");
-});
+    var contatore = white.text();
+    contatore --;
+    if (contatore >= 0) {
+        white.text(contatore);
+        removeColorClasses();
+        randomColor();
+    }
+  });
+}
 
-red.click(function() {
-  contatore += 1;
-  white.html(contatore);
-  white.toggleClass("whiteyellow" , "white");
-});
+function plusClick() {
+  var white = $(".white");
+  var red = $(".red");
+
+  red.click(function() {
+
+    var contatore = white.text();
+    contatore ++;
+    white.text(contatore);
+    removeColorClasses();
+    randomColor();
+  });
+}
+
+var colorClasses = [ "box-green", "box-blue", "box-purple" ];
+
+function removeColorClasses() {
+  var valueDiv = $("#box-value");
+
+  for (var i=0;i<colorClasses.length;i++) {
+      valueDiv.removeClass(colorClasses[i]);
+  }
+}
+
+function randomColor() {
+
+  var rndInd = getRandom(0, 2);
+  var colorClass = colorClasses[rndInd];
+
+  ("rndInd", rndInd);
+  ("colorClass", colorClass);
+
+  var valueDiv = $("#box-value");
+  valueDiv.addClass(colorClass);
+}
+
+function init() {
+  minusClick();
+  plusClick();
+}
+
+init();
